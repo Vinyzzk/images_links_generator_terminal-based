03381d976d23748b3651b19b8829c9dceb16892a
v <viniciuswolves@gmail.com>
2024-10-29 22:55:07 -0300

    feat!: new functions
    now you can just download images;
    now you can download imagen and upload immediately

--- app.py
+++ app.py
@@ -2,107 +2,162 @@ from time import sleep
 import json
 import os
 import requests
-import json
 import pandas as pd
-import openpyxl
 from PIL import Image
 import base64
+import urllib.request
+import re
 
+def create_folder(folder_path):
+    os.makedirs(folder_path, exist_ok=True)
+
+def download_images(url, folder_path, order):
+    image_path = os.path.join(folder_path, f"{order}.jpg")
+    urllib.request.urlretrieve(url, image_path)
+
+def get_images(mlb):
+    url = f"https://api.mercadolibre.com/items/{mlb}"
+    response = requests.get(url)
+    response_data = response.json()
+    variations = response_data.get("variations", [])
+    pictures = response_data.get("pictures", [])
+
+    if variations:
+        for variation in variations:
+            variation_name_parts = [
+                f"{attribute['name']}-{attribute['value_name']}"
+                for attribute in variation.get("attribute_combinations", [])
+            ]
+            variation_name = re.sub(r'[^a-zA-Z-]', '', "-".join(variation_name_parts))
+            folder_name = f"images/{mlb}-{variation_name}-{variation['id']}"
+            create_folder(folder_name)
+
+            for order, picture_id in enumerate(variation.get("picture_ids", []), start=1):
+                url = f"https://http2.mlstatic.com/D_{picture_id}-F.jpg"
+                download_images(url, folder_name, order)
+
+    elif pictures:
+        folder_name = f"images/{mlb}"
+        create_folder(folder_name)
+
+        for order, picture in enumerate(pictures, start=1):
+            url = f"https://http2.mlstatic.com/D_{picture['id']}-F.jpg"
+            download_images(url, folder_name, order)
+
+    print(f"[+] Imagens do {mlb} baixadas!")
+
+
+def get_images_by_list():
+    df = pd.read_excel("mlbs.xlsx")
+    mlbs = df["MLB"].values
+    
+    print("[!] Baixando imagens...")
+    if not mlbs:
+        print("[!] A lista de MLBs precisa ter ao menos um MLB.")
+        input("Pressione ENTER para finalizar")
+        return
+    
+    for mlb in mlbs:
+        get_images(mlb)
+
+    input("Pressione ENTER para finalizar")
 
 def check_token():
-    global token
-
-    # Check ImgBB API Token
-    with open("configs/config.json", "r") as file:
+    config_path = "configs/config.json"
+    with open(config_path, "r") as file:
         data = json.load(file)
         token = data.get("token")
 
     if not token:
         token = input("Informe o ImgBB API Token: ")
         data["token"] = token
-        with open("configs/config.json", "w") as file:
+        with open(config_path, "w") as file:
             json.dump(data, file, indent=4)
 
-    # TODO: adicionar uma verificacao se o token é valido ou nao, e pedir para inserir novamente
-    # TODO: adicionaru um botao para readicionar o token
-    # TODO: adicionar logs terminal-based, e retorno de interface de usuario conforme os arquivos forem sendo convertidos e upados
-    
-def default_folder():
-    os.makedirs("images", exist_ok=True)
-
-
-def create_folders():
-    os.makedirs("converted", exist_ok=True)
-    for folder in os.listdir("images"):
-        os.makedirs(f"converted/{folder}", exist_ok=True)
-
+    return token
 
 def convert_images():
+    create_folder("result")
     for folder in os.listdir("images"):
         folder_path = os.path.join("images", folder)
+        output_folder_path = os.path.join("result", folder)
+        create_folder(output_folder_path)
+
         if os.path.isdir(folder_path):
             for file in os.listdir(folder_path):
                 name, ext = os.path.splitext(file)
-                print(name, ext)
-                
                 input_file_path = os.path.join(folder_path, file)
-                output_folder_path = os.path.join("converted", folder)
                 output_file_path = os.path.join(output_folder_path, f"{name}.jpeg" if ext != ".png" else f"{name}.png")
 
-                if ext != ".png":
-                    img = Image.open(input_file_path).convert("RGB")
-                else:
-                    img = Image.open(input_file_path).convert("RGBA")
-                
+                img = Image.open(input_file_path).convert("RGB" if ext != ".png" else "RGBA")
                 img.save(output_file_path)
                 img.close()
+                print(f"[LOG] Imagem convertida: {name}")
 
-
-def upload_images():
+def upload_images(token):
     data = []
-
-    for folder in os.listdir("converted"):
-        folder_path = os.path.join("converted", folder)
+    for folder in os.listdir("result"):
+        folder_path = os.path.join("result", folder)
         if os.path.isdir(folder_path):
             links = []
-            links_bling_excel = []
             for img in os.listdir(folder_path):
                 name, ext = os.path.splitext(img)
                 if not img.endswith(".txt"):
                     with open(os.path.join(folder_path, img), "rb") as file:
-                        url = "https://api.imgbb.com/1/upload"
-                        payload = {
-                            "key": token,
-                            "image": base64.b64encode(file.read()),
-                        }
-                        res = requests.post(url, data=payload)
-                        res = res.json()
-                        res = res.get("data")
-                        res = res.get("url")
-                        links.append(f"{res}\n")
-                        links_bling_excel.append(res)
+                        payload = {"key": token, "image": base64.b64encode(file.read())}
+                        res = requests.post("https://api.imgbb.com/1/upload", data=payload).json()
+                        image_url = res["data"]["url"]
+                        links.append(image_url)
+                        print(f"[LOG] Imagem upada: {img} | Link: {image_url}")
 
-            my_separator = "|"
-            links.sort()  # Ordenar os links em ordem alfabética
-            links_bling = my_separator.join(links)  # Separar os links com o separador definido
-            links_bling_excel = my_separator.join(links_bling_excel)  # Separar os links com SKU com o separador definido
-
-            with open(os.path.join(folder_path, "url.txt"), "w+") as txt:
-                txt.writelines(links)
-            with open(os.path.join(folder_path, "url_bling.txt"), "w+") as txt2:
+            links_bling = "|".join(sorted(links))
+            with open(os.path.join(folder_path, "url.txt"), "w") as txt:
+                txt.writelines(f"{link}\n" for link in links)
+            with open(os.path.join(folder_path, "url_bling.txt"), "w") as txt2:
                 txt2.write(links_bling)
 
-            data.append({"SKU": folder, "Links": links_bling_excel})
+            data.append({"SKU": folder, "Links": links_bling})
 
-    df = pd.DataFrame(data)
-    df.to_excel("converted/result.xlsx", index=False, engine="openpyxl")
+    pd.DataFrame(data).to_excel("result/result.xlsx", index=False, engine="openpyxl")
 
+
+def input_mlb():
+    mlb = input("[>] Informe um MLB (ou digite 0 para sair): ").upper()
+    if mlb == "0":
+        return None
+    return mlb if mlb.startswith("MLB") else input_mlb()
 
 def main():
-    default_folder()
-    check_token()
-    create_folders()
-    convert_images()
-    upload_images()
+    create_folder("images")
+    create_folder("result")
+    token = check_token()
+    
+    option = int(input("""
+Escolha uma opção:
+[1] Converter e upar imagens
+[2] Baixar imagens de um MLB específico
+[3] Baixar, converter e upar imagens de um MLB
+Seleção: """))
+    
+    if option == 1:
+        convert_images()
+        upload_images(token)
 
-main()
+    elif option == 2:
+        while True:
+            mlb = input_mlb()
+            if mlb is None:
+                break
+            get_images(mlb)
+
+    elif option == 3:
+        while True:
+            mlb = input_mlb()
+            if mlb is None:
+                break
+            get_images(mlb)
+            convert_images()
+            upload_images(token)
+
+if __name__ == "__main__":
+    main()
